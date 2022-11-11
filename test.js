0de09af31bdf89c0f6c775ebbef0f12ae80e83cd
Numbeneer <117283181+Numbeneer@users.noreply.github.com>
2022-11-11 11:26:43 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
 function e() {
   var s = document.getElementById('demo')
-var value = parseInt(s.textContent, 10);    value = isNaN(value) ? 0 : value;    value++;    document.getElementById('demo').innerHTML = value;
+var value = parseInt(s.textContent, 10);    value = isNaN(value) ? 0 : value;    value.add(12);    document.getElementById('demo').innerHTML = value;
 }
