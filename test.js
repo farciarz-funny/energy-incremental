27bd95fa283fb18d0feeec99ab22627c0ff15a46
Numbeneer <117283181+Numbeneer@users.noreply.github.com>
2022-11-11 11:34:31 +0100

    Making ExapantaNum.js actually work
    I think it will after this and one more change

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
-function e() {
+function E(x){return new ExpantaNum(x)};
+function te() {
   var s = document.getElementById('demo')
 var value = parseInt(s.textContent, 10);    value = isNaN(value) ? 0 : value;    value.add(12);    document.getElementById('demo').innerHTML = value;
 }
