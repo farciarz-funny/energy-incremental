267f7622a961e551ec4647ed113d3d3ec54d9e04
Numbeneer <117283181+Numbeneer@users.noreply.github.com>
2022-11-12 16:08:39 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,5 @@
-function E(x){return new ExpantaNum(x)};
-let tesd = E(2)
+
 function te() {
   var s = document.getElementById('demo')
-var value = parseInt(s.textContent, 10);    value = isNaN(value) ? 0 : value;    value.add(tesd);    document.getElementById('demo').innerHTML = value;
+var value = parseInt(s.textContent, 10);    value = isNaN(value) ? 0 : value;    ExpantaNum.pow(value,10);    document.getElementById('demo').innerHTML = value;
 }
