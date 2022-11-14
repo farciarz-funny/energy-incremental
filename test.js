adfaf68f76da784a75d8f0b0a3aa8093c427ddd8
Numbeneer <117283181+Numbeneer@users.noreply.github.com>
2022-11-14 20:54:33 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 
 function te() {
-  var s = document.getElementById('test')
+  var s = document.getElementById('demo1')
 var q = ExpantaNum.add(player.KE,1)
 s.innerHTML = "you have " + q + " kinetic energy"
 
