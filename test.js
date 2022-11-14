1888fdb5f62585a01f437528f396fbf4882173da
Numbeneer <117283181+Numbeneer@users.noreply.github.com>
2022-11-14 18:07:40 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,7 @@
 
 function te() {
-  var s = document.getElementById('demo')
-var value = s.textContent
-var q = ExpantaNum.pow(value,10)
-s.innerHTML = q
+  var s = document.getElementById('demo1')
+var q = ExpantaNum.pow(10,10)
+s.innerHTML = "you have " + q + " kinetic energy"
 
 }
