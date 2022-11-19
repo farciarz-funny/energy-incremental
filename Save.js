0b81786c9d9941d7c7961a279fb8a719a74341f8
Numbeneer <117283181+Numbeneer@users.noreply.github.com>
2022-11-19 14:28:59 +0100

    Add files via upload

---
+++ Save.js
@@ -0,0 +1,92 @@
+var player = {}
+function E(x){return new ExpantaNum(x)};
+
+function ex(x){
+
+    let nx = new E(0);
+
+    nx.array = x.array;
+
+    nx.sign = x.sign;
+
+    nx.layer = x.layer;
+
+    return nx;
+
+}
+function wipe() {
+
+    player = {
+    KE: E(0),
+    M: E(0),
+    V: E(0),
+    P: E(0)
+    }
+    }
+    function save(){
+
+    if (localStorage.getItem("Save") == '') wipe()
+
+    localStorage.setItem("Save",btoa(JSON.stringify(player)))
+
+}
+
+function load(x){
+
+    if(typeof x == "string" & x != ''){
+
+        loadPlayer(JSON.parse(atob(x)))
+
+    } else {
+
+        wipe()
+
+    }
+
+}
+
+function exporty() {
+
+    save();
+
+    let file = new Blob([btoa(JSON.stringify(player))], {type: "text/plain"})
+
+    window.URL = window.URL || window.webkitURL;
+
+    let a = document.createElement("a")
+
+    a.href = window.URL.createObjectURL(file)
+
+    a.download = "Energy Incremental Save.txt"
+
+    a.click()
+
+}
+
+function importy() {
+
+    let loadgame = prompt("Paste in your save WARNING: WILL OVERWRITE YOUR CURRENT SAVE")
+
+    if (loadgame != null) {
+
+        load(loadgame)
+
+    }
+
+}
+function loadPlayer(load) {
+player.KE = ex(load.KE)
+player.M = ex(load.M)
+player.V = ex(load.V)
+player.P = ex(load.P)
+}
+function loadGame() {
+
+    wipe()
+
+    load(localStorage.getItem("Save"))
+
+
+    setInterval(save,1000)
+
+}
