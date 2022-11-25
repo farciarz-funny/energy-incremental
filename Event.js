6305fffcf55d6b7cfdcbfcfa03bd7eca3e951c46
Numbeneer <fe000000e308@gmail.com>
2022-11-25 15:39:58 +0100

    update
    with more things but im not sure if i fixed what i want

--- Event.js
+++ Event.js
@@ -1,10 +1,27 @@
 import {te, mass, vel, pow} from './Main.js'
-import { loadGame } from './Save.js'
+import { loadGame, wipe, importy, exporty, exporty_copy } from './Save.js'
 var s = document.getElementById("demo0")
+var wi = document.getElementById("wipe")
+var im = document.getElementById("import")
+var ex = document.getElementById("export")
+var exc = document.getElementById("exportc")
 s.addEventListener('click', () => {
 te()
 })
 var body = document.body
 body.addEventListener('load', () => {
     loadGame()
+})
+wi.addEventListener('click', () => {
+    wipe()
+})
+im.addEventListener('click', () => {
+    importy()
+})
+ex.addEventListener('click', () => {
+    exporty()
+})
+
+exc.addEventListener('click', () => {
+    exporty_copy()
 })
