a0f44e5df8c2eee214e7c77387e663820ca6b7b3
Numbeneer <117283181+Numbeneer@users.noreply.github.com>
2022-11-25 21:33:26 +0100

    update Event.js

--- Event.js
+++ Event.js
@@ -1,4 +1,4 @@
-import {tet, mass, vel, pow} from './Main.js'
+import {te, mass, vel, pow} from './Main.js'
 import { loadGame, wipe, importy, exporty } from './Save.js'
 var s = document.getElementById("demo0")
 var wi = document.getElementById("wipe")
@@ -7,10 +7,9 @@ var ex = document.getElementById("export")
 s.addEventListener('click', () => {
 te()
 })
-var body = document.body
 window.addEventListener('load', () => {
+  console.log("loodaaeed")
     loadGame()
-    console.log("loodaaeed")
 })
 wi.addEventListener('click', () => {
     wipe()
@@ -20,7 +19,4 @@ im.addEventListener('click', () => {
 })
 ex.addEventListener('click', () => {
     exporty()
-})
-window.addEventListener('load', () => {
-  console.log('loaded')
 })
